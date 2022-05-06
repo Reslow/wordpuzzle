431b54e74dd0e5f063e9749318dfd2fdd01b713c
Fanny Reslow <reslowf@gmail.com>
2022-05-06 18:09:29 +0200

    puzzle makign

--- index.js
+++ index.js
@@ -16,11 +16,41 @@ function getrandomwords(data) {
   pairOfWords(words);
   createInput(words);
 }
+
 function createInput(words) {
   let inputfield = document.createElement("input");
+  let btn = document.createElement("button");
   inputfield.setAttribute("maxlength", words.startWord.length);
 
+  btn.innerHTML = "add";
+  btn.setAttribute("type", "button");
   form.appendChild(inputfield);
+  form.appendChild(btn);
+
+  btn.addEventListener("click", () => {
+    checkInput(inputfield.value, words);
+  });
+}
+
+function checkInput(value, words) {
+  // kolla om alla utom en bokstav skiljer sig.
+  console.log(value);
+  let valueUpper = value.toUpperCase();
+  let startwordinArray = words.startWord.split("");
+  let inputArray = valueUpper.split("");
+  console.log(startwordinArray);
+  console.log(inputArray);
+  hasWordChanged(startwordinArray, inputArray);
+}
+
+function hasWordChanged(startwordArray, inputarray) {
+  console.log(startwordArray);
+  console.log(inputarray);
+
+  for (let i = 0; i < startwordArray.length; i++) {
+    if (startwordArray[i] === inputarr) console.log(startwordArray[i]);
+    console.log(inputarray[i]);
+  }
 }
 
 function pairOfWords(words) {
