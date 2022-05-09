1c8565ec2bfbb0c2b209f3e7824bf0bf695024f9
Fanny Reslow <reslowf@gmail.com>
2022-05-09 11:08:02 +0200

    adding comments for code and cleaning up code

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// connecting to html
 let startW = document.getElementById("startW");
 let endW = document.getElementById("endW");
 let form = document.getElementById("form");
 
+// fetch words from words.json
 async function getWords() {
   let response = await fetch("/words.json");
   const data = await response.json();
@@ -9,58 +11,54 @@ async function getWords() {
   getrandomwords(data);
 }
 
+// select a random pair of words from words.json
 function getrandomwords(data) {
   let words = data[Math.floor(Math.random() * data.length)];
-  pairOfWords(words);
+  // display words in html
+  startW.innerText = words.startWord;
+  endW.innerText = words.endWord;
+
   createInput(words);
 }
 
+// create inputfields from numbers of letters
+
 function createInput(words) {
   let number = words.startWord.length;
+  //  for each letter create input
   for (let i = 0; i < number; i++) {
     let inputfield = document.createElement("input");
     inputfield.setAttribute("maxlength", 1);
     form.appendChild(inputfield);
-    checkInput(inputfield.value, words);
   }
-
+  // create a btn
   let btn = document.createElement("button");
   btn.innerHTML = "try";
   btn.setAttribute("type", "button");
   form.appendChild(btn);
-  btn.addEventListener("click", () => {});
+  btn.addEventListener("click", () => {
+    console.log("click");
+    checkInput(inputfield.value, words);
+  });
 }
 
+// turn value of inpuit to uppercases, then turn strings to arrays and compare them
 function checkInput(value, words) {
-  // kolla om alla utom en bokstav skiljer sig.
-  console.log(value);
+  // to uppercases and str into arrays
   let valueUpper = value.toUpperCase();
   let startwordinArray = words.startWord.split("");
   let inputArray = valueUpper.split("");
-  console.log(startwordinArray);
-  console.log(inputArray);
-  hasWordChanged(startwordinArray, inputArray);
-}
 
-function hasWordChanged(startwordArray, inputarray) {
-  console.log(startwordArray);
-  console.log(inputarray);
-
-  for (let i = 0; i < startwordArray.length; i++) {
-    if (startwordArray[i] !== inputarray[i]) {
-      console.log(startwordArray[i]);
-      console.log(inputarray[i]);
+  // go through the listsand compare each letter if they matches
+  for (let i = 0; i < startwordinArray.length; i++) {
+    if (startwordinArray[i] !== inputArray[i]) {
+      console.log(startwordinArray[i]);
+      console.log(inputArray[i]);
       console.log("mismatch");
     } else {
-      console.log("we have a mtach");
+      console.log("we have a match");
     }
   }
-}
-
-function pairOfWords(words) {
-  // words is an object with id, startword and endword
-  startW.innerText = words.startWord;
-  endW.innerText = words.endWord;
 }
 
 function handleInput(e) {
@@ -70,5 +68,8 @@ function handleInput(e) {
     input.nextElementSibling.focus();
   }
 }
-getWords();
+
+// listen for input
 form.addEventListener("input", handleInput);
+
+getWords();
